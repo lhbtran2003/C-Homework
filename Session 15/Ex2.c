9d05385667161c179f2b52524fcf2e185f71bfeb
lhbtran2003 <lhbtran2003@gmail.com>
2023-12-26 05:47:01 +0700

    up code

--- Session 15/Ex2.c
+++ Session 15/Ex2.c
@@ -1,28 +1,70 @@
 #include<stdio.h>
 #include<stdlib.h>
-void input(int y, int x);
-int main(){
-    int y, x;
-    input(y, x);
-
-
-
-
+void swap_y(int** arr, int hang_1, int hang_2, int cot)
+{
+    for(int i=0; i<cot; i++) {
+        int temp = arr[hang_1][i];
+        arr[hang_1][i] = arr[hang_2][i];
+        arr[hang_2][i] = temp;
+    }
 }
-void input(int y, int x) {
-    int i, j;
+
+void swap_x(int** arr, int cot_1, int cot_2, int hang)
+{
+    for(int i = 0; i < hang; i++) {
+        int temp = arr[i][cot_1];
+        arr[i][cot_1] = arr[i][cot_2];
+        arr[i][cot_2] = temp;
+    }
+}
+
+void input(int** arr, int y, int x)
+{
+    printf("Nhap gia tri tung phan tu:\n");
+    for (int i = 0; i < y; i++) {
+        for (int j = 0; j < x; j++) {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+}
+
+
+int main() {
+    int y, x;
     printf("Nhap so hang:");
     scanf("%d", &y);
     printf("Nhap so cot:");
     scanf("%d", &x);
-    int *nums;
-    //Cấp phát bộ nhớ động và gán vào biến con trỏ
-    nums = malloc(sizeof(int) * x * y);
-    printf("Nhap gia tri tung phan tu:");
-    for (int i = 0; i < y; i++) {
-        for (int j = 0; j < x; j++) {
-            scanf("%d", &nums[i * x + j]);
-        }
+
+    int** arr = (int**)malloc(y * sizeof(int*));
+    for(int i = 0; i < y; i++) {
+        arr[i] = (int*)malloc(x * sizeof(int));
     }
+
+    input(arr, y, x);
+
+    int r1, r2, c1, c2;
+    printf("Nhap hai hang can hoan vi: ");
+    scanf("%d %d", &r1, &r2);
+    swap_y(arr, r1, r2, x);
+
+    printf("Nhap hai cot can hoan vi: ");
+    scanf("%d %d", &c1, &c2);
+    swap_x(arr, c1, c2, y);
+
+    printf("Mang sau khi hoan vi:\n");
+    for(int i = 0; i < y; i++) {
+        for(int j = 0; j < x; j++) {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+
+    for(int i = 0; i < y; i++) {
+        free(arr[i]);
+    }
+    free(arr);
+
+    return 0;
 }
-void swap_y(int *)
+
---
+++ Session 15/Ex4.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int is_prime(int n) {
+    if (n <= 1) return 0;
+    if (n <= 3) return 1;
+
+    if (n % 2 == 0 || n % 3 == 0) return 0;
+
+    for (int i = 5; i * i <= n; i = i + 6)
+        if (n % i == 0 || n % (i + 2) == 0)
+            return 0;
+
+    return 1;
+}
+
+int main() {
+    int hang, cot;
+    printf("Nhap so hang: ");
+    scanf("%d", &hang);
+    printf("Nhap so cot: ");
+    scanf("%d", &cot);
+
+    int** arr = (int**)malloc(hang * sizeof(int*));
+    for(int i = 0; i < cot; i++) {
+        arr[i] = (int*)malloc(cot * sizeof(int));
+    }
+
+    printf("Nhap cac phan tu cua mang:\n");
+    for(int i = 0; i < hang; i++) {
+        for(int j = 0; j < cot; j++) {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+
+    int max_primes = 0;
+    int row_index = -1;
+    for(int i = 0; i < hang; i++) {
+        int prime_count = 0;
+        for(int j = 0; j < cot; j++) {
+            if(is_prime(arr[i][j])) {
+                prime_count++;
+            }
+        }
+        if(prime_count >= max_primes) {
+            max_primes = prime_count;
+            row_index = i;
+        }
+    }
+
+    printf("Hang co nhieu so nguyen to nhat la: %d\n", row_index);
+
+    for(int i = 0; i < hang; i++) {
+        free(arr[i]);
+    }
+    free(arr);
+
+    return 0;
+}
+
